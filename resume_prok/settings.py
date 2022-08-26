3dbd1bf2ff04327cdb7e17d4ac63325ed123d3d3
Murat Kairzhanov <murat99010@gmail.com>
2022-08-26 18:43:06 +0600

    frontend

--- resume_prok/settings.py
+++ resume_prok/settings.py
@@ -143,3 +143,5 @@ MEDIA_ROOT = BASE_DIR/'mediafiles'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
--- resume_prok/urls.py
+++ resume_prok/urls.py
@@ -17,13 +17,18 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-import main
+from django.conf.urls import url
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('main.urls',namespace = 'main'))
+    path('',include('main.urls',namespace = 'main')),
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
 ]
 
 if settings.DEBUG:
-    urlpatterns+=static(settings.STATIC_URL, document_root = settings.STATIC_URL)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
+    urlpatterns+=static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
